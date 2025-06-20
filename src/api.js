940085624a1b9c56a000bd11610fbbb2845c8ec5
RSKD 2025 <rskd2025@example.com>
2025-06-20 11:36:00 +0900

    update baseURL ke ngrok baru

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://84d7-36-83-213-135.ngrok-free.app', // sesuai ngrok aktif
+  baseURL: ' https://9228-36-83-213-135.ngrok-free.app', // sesuai ngrok aktif
 });
 
 export default api;
