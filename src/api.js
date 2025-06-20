eaa219a7db990f9bc327da926d14ffc0697a69ad
RSKD 2025 <rskd2025@example.com>
2025-06-20 11:27:28 +0900

    update baseURL ngrok

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://a68d-36-83-213-135.ngrok-free.app', // sesuai ngrok aktif
+  baseURL: 'https://84d7-36-83-213-135.ngrok-free.app', // sesuai ngrok aktif
 });
 
 export default api;
