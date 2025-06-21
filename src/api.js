0a5e0fcba36edeeb3e8ce0dfeb232a969859a5c3
RSKD 2025 <rskd2025@example.com>
2025-06-21 19:14:58 +0900

    update ngrok id

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://3f7d-36-83-213-135.ngrok-free.app',
+  baseURL: 'https://6c1f-36-83-213-135.ngrok-free.app',
   withCredentials: true, // ✅ WAJIB!
   });
 
