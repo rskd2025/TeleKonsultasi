037f97384e0dfc9cd1e843678fec8d79a96921e0
RSKD 2025 <rskd2025@example.com>
2025-06-24 06:55:12 +0900

    update appi.js with ngrok credential

--- src/api.js
+++ src/api.js
@@ -2,8 +2,11 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://7a48-36-83-213-135.ngrok-free.app',
-  withCredentials: true, // ✅ WAJIB!
-  'ngrok-skip-browser-warning': 'true'
-  });
+  withCredentials: true,
+  headers: {
+    'ngrok-skip-browser-warning': 'true'
+  }
+});
 
 export default api;
+
