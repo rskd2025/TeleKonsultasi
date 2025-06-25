823e798bb2d9b91918af7a0eb073f92648f4a80e
RSKD 2025 <rskd2025@example.com>
2025-06-25 13:27:43 +0900

    update base url

--- src/api.js
+++ src/api.js
@@ -1,11 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://7a48-36-83-213-135.ngrok-free.app',
+  baseURL: 'https://tekosiwalima.my.id',
   withCredentials: true,
-  headers: {
-    'ngrok-skip-browser-warning': 'true'
-  }
 });
 
 export default api;
